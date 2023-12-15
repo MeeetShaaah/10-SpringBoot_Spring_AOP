8b5ea4247b435fd1b073ca85d2382bcf80895cbb
Meet Shah <m.shah@student.fdu.edu>
2023-12-15 14:15:56 -0500

    Start With Spring APO

---
+++ SpringAOPDemo/src/main/java/com/luv2code/SpringAOPDemo/SpringAopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.SpringAOPDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopDemoApplication.class, args);
+	}
+
+}
---
+++ SpringAOPDemo/src/test/java/com/luv2code/SpringAOPDemo/SpringAopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.SpringAOPDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
