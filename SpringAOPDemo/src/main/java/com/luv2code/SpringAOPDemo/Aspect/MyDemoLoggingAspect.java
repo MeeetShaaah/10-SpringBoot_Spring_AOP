53af35cea22e6c8124e7794836c8dc93631e2829
Meet Shah <m.shah@student.fdu.edu>
2023-12-18 12:14:24 -0500

    Code for PontCutDeclaration.

--- SpringAOPDemo/src/main/java/com/luv2code/SpringAOPDemo/Aspect/MyDemoLoggingAspect.java
+++ SpringAOPDemo/src/main/java/com/luv2code/SpringAOPDemo/Aspect/MyDemoLoggingAspect.java
@@ -2,12 +2,17 @@ package com.luv2code.SpringAOPDemo.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-    @Before("execution(public void com.luv2code.SpringAOPDemo.DAO.*.*(*))")
+
+    @Pointcut("execution(public void com.luv2code.SpringAOPDemo.DAO.*.*(*)))")
+    private void pointCutDeclaration(){}
+
+    @Before("pointCutDeclaration()")
     public void beforeAddingAccount() {
        System.out.println("\n====> Code in this Aspect block will run @Before running the addAccount()."); 
     }
